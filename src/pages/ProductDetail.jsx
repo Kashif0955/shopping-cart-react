5a450f510f46e61bb23c8b22322029f8424e7a32
Malik Kashif <kashif2112a@aptechsite.net>
2024-09-14 17:02:46 +0500

    project has been completed

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -140,7 +140,7 @@ function ProductDetail({ handler }) {
                   >
                     ${product.price}
                   </span>
-                  <button
+                  {/* <button
                     className={`flex ml-auto text-white border-0 py-2 px-6 focus:outline-none ${
                       theme === "light"
                         ? "bg-indigo-500 hover:bg-indigo-600"
@@ -158,7 +158,7 @@ function ProductDetail({ handler }) {
                     }
                   >
                     Add to Cart
-                  </button>
+                  </button> */}
                   <button
                     className={`rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center ${
                       theme === "light" ? "text-gray-500" : "text-gray-400"
